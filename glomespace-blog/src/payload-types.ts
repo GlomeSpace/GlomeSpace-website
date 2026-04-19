e37561f5126c59cdde93f7ea1afd87cb1c1203d2
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2026-04-20 00:33:53 +0100

    Configuring file upload
    Signed-off-by: Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>

--- glomespace-blog/src/payload-types.ts
+++ glomespace-blog/src/payload-types.ts
@@ -161,6 +161,7 @@ export interface User {
 export interface Media {
   id: number;
   alt: string;
+  caption?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -172,6 +173,32 @@ export interface Media {
   height?: number | null;
   focalX?: number | null;
   focalY?: number | null;
+  sizes?: {
+    thumbnail?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+    card?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+    hero?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+  };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -203,7 +230,7 @@ export interface Blog {
   author: number | User;
   publishDate: string;
   status: 'draft' | 'published';
-  thumbnail: number | Media;
+  thumbnail?: (number | null) | Media;
   tags?:
     | {
         tag?: string | null;
@@ -326,6 +353,7 @@ export interface UsersSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
+  caption?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
@@ -337,6 +365,40 @@ export interface MediaSelect<T extends boolean = true> {
   height?: T;
   focalX?: T;
   focalY?: T;
+  sizes?:
+    | T
+    | {
+        thumbnail?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+        card?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+        hero?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+      };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
--- glomespace-blog/src/payload.config.ts
+++ glomespace-blog/src/payload.config.ts
@@ -40,7 +40,7 @@ export default buildConfig({
         media: true, 
       },
       token: process.env.BLOB_READ_WRITE_TOKEN_READ_WRITE_TOKEN,
-      clientUploads: false, 
+      clientUploads: true, 
     }),
   ],
 })
